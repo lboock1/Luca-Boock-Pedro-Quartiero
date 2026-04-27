dd283aa07f09fc6030aacc165afaaecdba34b786
Luca Boock <lboock@yahoo.com>
2026-04-27 16:52:33 -0300

    funcao guardar_dado

--- funcoes.py
+++ funcoes.py
@@ -3,5 +3,12 @@ def rolar_dados(quantidade):
     resultado = []
     for _ in range(quantidade):
         resultado.append(random.randint(1, 6))
-    return resultado    
+    return resultado   
+
+def guardar_dado(dados_r, dados_g, indice):
+    dado= dados_r.pop(indice)
+    dados_g.append(dado)
+    return [dados_r, dados_g]
+
+ 
     
