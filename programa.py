7653597f091d519d10d22c8ea1e7f05e01d03bf5
Luca Boock <lboock@yahoo.com>
2026-04-30 13:41:23 -0300

    programa

--- programa.py
+++ programa.py
@@ -12,6 +12,8 @@ cartela = {
     }
 }
 
+imprime_cartela(cartela)
+
 for rodada in range(12):
     dados_rolados = rolar_dados(5)
     dados_guardados = []
@@ -22,28 +24,22 @@ for rodada in range(12):
         print(f"Dados guardados: {dados_guardados}")
         print("Digite 1 para guardar um dado, 2 para remover um dado, 3 para rerrolar, 4 para ver a cartela ou 0 para marcar a pontuação:")
 
-        opcao = input(">")
+        opcao = input()
 
         if opcao == "1":
             print("Digite o índice do dado a ser guardado (0 a 4):")
-            try:
-                indice = int(input(">"))
-                if 0 <= indice < len(dados_rolados):
-                    guardar_dado(dados_rolados, dados_guardados, indice)
-                else:
-                    print("Opção inválida. Tente novamente.")
-            except:
+            indice = int(input())
+            if 0 <= indice < len(dados_rolados):
+                guardar_dado(dados_rolados, dados_guardados, indice)
+            else:
                 print("Opção inválida. Tente novamente.")
 
         elif opcao == "2":
             print("Digite o índice do dado a ser removido (0 a 4):")
-            try:
-                indice = int(input(">"))
-                if 0 <= indice < len(dados_guardados):
-                    remover_dado(dados_rolados, dados_guardados, indice)
-                else:
-                    print("Opção inválida. Tente novamente.")
-            except:
+            indice = int(input())
+            if 0 <= indice < len(dados_guardados):
+                remover_dado(dados_rolados, dados_guardados, indice)
+            else:
                 print("Opção inválida. Tente novamente.")
 
         elif opcao == "3":
@@ -57,32 +53,34 @@ for rodada in range(12):
             imprime_cartela(cartela)
 
         elif opcao == "0":
-            print("Digite a combinação desejada:")
-            categoria = input(">")
+            while True:
+                print("Digite a combinação desejada:")
+                categoria = input()
 
-            if categoria in cartela['regra_simples']:
-                categoria = str(categoria)
+                if categoria in [str(i) for i in range(1,7)]:
+                    if cartela['regra_simples'][int(categoria)] != -1:
+                        print("Essa combinação já foi utilizada.")
+                    else:
+                        dados_totais = dados_rolados + dados_guardados
+                        faz_jogada(dados_totais, categoria, cartela)
+                        break
 
-            if (categoria not in cartela['regra_avancada'] 
-                and categoria not in [str(i) for i in range(1,7)]):
-                print("Combinação inválida. Tente novamente.")
-                continue
+                elif categoria in cartela['regra_avancada']:
+                    if cartela['regra_avancada'][categoria] != -1:
+                        print("Essa combinação já foi utilizada.")
+                    else:
+                        dados_totais = dados_rolados + dados_guardados
+                        faz_jogada(dados_totais, categoria, cartela)
+                        break
 
-            if categoria in [str(i) for i in range(1,7)]:
-                if cartela['regra_simples'][int(categoria)] != -1:
-                    print("Essa combinação já foi utilizada.")
-                    continue
-            else:
-                if cartela['regra_avancada'][categoria] != -1:
-                    print("Essa combinação já foi utilizada.")
-                    continue
+                else:
+                    print("Combinação inválida. Tente novamente.")
 
-            dados_totais = dados_rolados + dados_guardados
-            faz_jogada(dados_totais, categoria, cartela)
             break
 
         else:
             print("Opção inválida. Tente novamente.")
+
 
 imprime_cartela(cartela)
 
